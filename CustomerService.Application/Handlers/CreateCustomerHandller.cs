8a01bdb349928cd2ea911f68ed63ccc4bf253d5a
unknown <zeynepozturkmen62@gmail.com>
2025-10-03 23:49:22 +0300

    Added ValidateTurkishIdentityNumber method

--- CustomerService.Application/Handlers/CreateCustomerHandller.cs
+++ CustomerService.Application/Handlers/CreateCustomerHandller.cs
@@ -25,8 +25,8 @@ namespace CustomerService.Application.Handlers
                 throw new Exception("Customer must be 18+");
 
             // National ID validation
-            if (request.NationalId.Length != 11)
-                throw new Exception("Invalid National ID");
+            if (!ValidateTurkishIdentityNumber(request.NationalId))
+            throw new Exception("Invalid National ID");
 
             if (request.CustomerType == CustomerType.Corporate && String.IsNullOrEmpty(request.TaxNumber))
                 throw new Exception("The tax number is mandatory for corporate customers");
@@ -56,5 +56,50 @@ namespace CustomerService.Application.Handlers
            
             return customer;
         }
+
+        public static bool ValidateTurkishIdentityNumber(string identityNumber)
+        {
+            // Uzunluk ve null kontrolü
+            if (string.IsNullOrWhiteSpace(identityNumber) || identityNumber.Length != 11)
+                return false;
+
+            // Rakamdan oluşmalı
+            foreach (char c in identityNumber)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+
+            // İlk hanesi 0 olamaz
+            if (identityNumber[0] == '0')
+                return false;
+
+            // Karakterleri tek tek sayıya dönüştürelim
+            int[] digits = new int[11];
+            for (int i = 0; i < 11; i++)
+            {
+                digits[i] = int.Parse(identityNumber[i].ToString());
+            }
+
+            // 1,3,5,7,9 toplamı
+            int oddSum = digits[0] + digits[2] + digits[4] + digits[6] + digits[8];
+            // 2,4,6,8 toplamı
+            int evenSum = digits[1] + digits[3] + digits[5] + digits[7];
+
+            // 10. hane kontrolü
+            int checkDigit10 = ((oddSum * 7) - evenSum) % 10;
+            if (checkDigit10 != digits[9])
+                return false;
+
+            // 11. hane kontrolü
+            int checkDigit11 = (digits[0] + digits[1] + digits[2] + digits[3] + digits[4] +
+                                digits[5] + digits[6] + digits[7] + digits[8] + digits[9]) % 10;
+
+            if (checkDigit11 != digits[10])
+                return false;
+
+            return true;
+        }
+
     }
 }
